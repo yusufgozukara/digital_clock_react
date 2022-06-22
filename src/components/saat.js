5ee75119bf8303e537963e71e8f97037ede6b5bc
yusufgozukara <yusufgozukara@gmail.com>
2022-06-22 17:46:49 +0300

    first

---
+++ src/components/saat.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+
+
+
+
+
+function Saat(){
+const [timer, setTimer] = useState();
+
+
+setInterval(() => {
+    let time = new Date().toLocaleTimeString();
+    setTimer(time)
+});
+
+return(
+    <p>{timer}</p>
+)
+
+
+
+}
+
+
+
+
+
+
+
+export default Saat;
